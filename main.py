1efcfb805b04882d98dd7c27f912341a2b2612d5
olega211 <106370448+olega211@users.noreply.github.com>
2022-07-23 02:27:17 +0700

    f

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 import requests
 from config import TOKEN
-
+import time
+import math
+from progress import progressBar
 
 
 def get_group_info(group_id):
     """Get group info"""
     fields = 'description,members_count'
-    req = requests.get(f'https://api.vk.com/method/groups.getById?v=5.131&group_id={group_id}&fields={fields}&access_token={TOKEN}')
+    req = requests.get(
+        f'https://api.vk.com/method/groups.getById?v=5.131&group_id={group_id}&fields={fields}&access_token={TOKEN}')
+    print(req.json())
     return req.json()['response'][0]
+
 
 def get_group_members(group_id, count):
     """Get group members"""
+
+    length = math.ceil(count / 400)
+    data = []
+    offset = 0
+    num = 0
     
-    offset = 401
-    fields = 'city,bdate,contacts'
-    req = requests.get(f'https://api.vk.com/method/groups.getMembers?v=5.131&group_id={group_id}&offset={offset}&fields={fields}&access_token={TOKEN}')
-    return req.json()
+    # print progress bar
+    progressBar(num, length, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-c = 0
-for i in get_group_members('thai_island_submariner', '0')['response']['items']:
-    print(i)
-    c += 1
+    while offset <= count:
+        fields = 'city,bdate,contacts'
+        req = requests.get(
+            f'https://api.vk.com/method/groups.getMembers?v=5.131&group_id={group_id}&offset={offset}&fields={fields}&access_token={TOKEN}')
+        offset += 400
+        num += 1
+        for i in req.json()['response']['items']:
+            if i not in data:
+                data.append(i)
+        time.sleep(5)
+        
+        # + progress bar
+        progressBar(num, length, prefix = 'Progress:', suffix = 'Complete', length = 50)
+        
+    return data
 
-print(c)
 
 id_group = 'thai_island_submariner'
+
+# GET BASIC INFO ABOUT GROUP
 try:
     info = get_group_info(id_group)
     count = info['members_count']
@@ -37,4 +57,12 @@ try:
 except Exception as e:
     print('Error. Get group info')
     print(e)
-
+    
+# GET MEMBERS
+try:
+    members_data = get_group_members(id_group, count)
+    print(members_data)
+    
+except Exception as e:
+    print('Error. Get members')
+    print(e)
--- progress.py
+++ progress.py
@@ -1,6 +1,6 @@
 import time
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+def progressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -21,13 +21,3 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     # Print New Line on Complete
     if iteration == total:
         print()
-# A List of Items
-items = list(range(0, 57))
-l = len(items)
-# Initial call to print 0% progress
-printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-for i, item in enumerate(items):
-    # Do stuff...
-    time.sleep(0.1)
-    # Update Progress Bar
-    printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
